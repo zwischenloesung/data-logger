cedce5b6416c40db5e516d8fd873c89056fbb90b
Michael Lustenberger <mic@inofix.ch>
2017-03-15 22:34:06 +0100

    Xml-ize the sensor data
    Remove pot. placeholder for carriage return and replace the newline
    placeholder with a real newline.

--- upload.py
+++ upload.py
@@ -44,6 +44,8 @@ except TypeError, KeyError:
     file_content = data.value.__repr__()
     file_content = a.strip("'")
     file_content = a.strip('"')
+    file_content = re.sub("\\\\r", "", a)
+    file_content = re.sub("\\\\n", "\n", a)
     with open(data_file, "w") as of:
         of.write(file_content)
     with open(data_archive, "w") as of:
